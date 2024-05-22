028bc0e7a81979c0facb8dc459592874f498ee21
povilas1565 <pavel.ryzhkov.99@mail.ru>
2024-05-22 12:47:27 +0300

    Gmorse based on ggmorse

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Morse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbInput = new System.Windows.Forms.GroupBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.rbnEncode = new System.Windows.Forms.RadioButton();
+            this.grbOutput = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.rbnDecode = new System.Windows.Forms.RadioButton();
+            this.grbTypes = new System.Windows.Forms.GroupBox();
+            this.grbOptions = new System.Windows.Forms.GroupBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.grbInput.SuspendLayout();
+            this.grbOutput.SuspendLayout();
+            this.grbTypes.SuspendLayout();
+            this.grbOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbInput
+            // 
+            this.grbInput.Controls.Add(this.txtInput);
+            this.grbInput.Location = new System.Drawing.Point(12, 12);
+            this.grbInput.Name = "grbInput";
+            this.grbInput.Size = new System.Drawing.Size(1053, 420);
+            this.grbInput.TabIndex = 0;
+            this.grbInput.TabStop = false;
+            this.grbInput.Text = "Input";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(6, 25);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(1040, 389);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // rbnEncode
+            // 
+            this.rbnEncode.AutoSize = true;
+            this.rbnEncode.Checked = true;
+            this.rbnEncode.Location = new System.Drawing.Point(6, 25);
+            this.rbnEncode.Name = "rbnEncode";
+            this.rbnEncode.Size = new System.Drawing.Size(169, 24);
+            this.rbnEncode.TabIndex = 1;
+            this.rbnEncode.TabStop = true;
+            this.rbnEncode.Text = "Text to Morse code";
+            this.rbnEncode.UseVisualStyleBackColor = true;
+            this.rbnEncode.CheckedChanged += new System.EventHandler(this.rbnEncode_CheckedChanged_1);
+            // 
+            // grbOutput
+            // 
+            this.grbOutput.Controls.Add(this.txtOutput);
+            this.grbOutput.Location = new System.Drawing.Point(12, 438);
+            this.grbOutput.Name = "grbOutput";
+            this.grbOutput.Size = new System.Drawing.Size(1053, 404);
+            this.grbOutput.TabIndex = 2;
+            this.grbOutput.TabStop = false;
+            this.grbOutput.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(7, 25);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(1040, 373);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // rbnDecode
+            // 
+            this.rbnDecode.AutoSize = true;
+            this.rbnDecode.Location = new System.Drawing.Point(6, 55);
+            this.rbnDecode.Name = "rbnDecode";
+            this.rbnDecode.Size = new System.Drawing.Size(172, 24);
+            this.rbnDecode.TabIndex = 3;
+            this.rbnDecode.TabStop = true;
+            this.rbnDecode.Text = "Morse Code to Text";
+            this.rbnDecode.UseVisualStyleBackColor = true;
+            // 
+            // grbTypes
+            // 
+            this.grbTypes.Controls.Add(this.rbnEncode);
+            this.grbTypes.Controls.Add(this.rbnDecode);
+            this.grbTypes.Location = new System.Drawing.Point(218, 25);
+            this.grbTypes.Name = "grbTypes";
+            this.grbTypes.Size = new System.Drawing.Size(188, 89);
+            this.grbTypes.TabIndex = 4;
+            this.grbTypes.TabStop = false;
+            this.grbTypes.Text = "Encode or Decode";
+            // 
+            // grbOptions
+            // 
+            this.grbOptions.Controls.Add(this.btnCopy);
+            this.grbOptions.Controls.Add(this.btnPlay);
+            this.grbOptions.Controls.Add(this.grbTypes);
+            this.grbOptions.Location = new System.Drawing.Point(1071, 12);
+            this.grbOptions.Name = "grbOptions";
+            this.grbOptions.Size = new System.Drawing.Size(414, 830);
+            this.grbOptions.TabIndex = 5;
+            this.grbOptions.TabStop = false;
+            this.grbOptions.Text = "Options";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(6, 36);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(206, 78);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play Morse Code Audio";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(6, 120);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(206, 78);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy Output to Clipboard";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1501, 853);
+            this.Controls.Add(this.grbOptions);
+            this.Controls.Add(this.grbOutput);
+            this.Controls.Add(this.grbInput);
+            this.Name = "Form1";
+            this.Text = "Morse Code Translator";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.grbInput.ResumeLayout(false);
+            this.grbInput.PerformLayout();
+            this.grbOutput.ResumeLayout(false);
+            this.grbOutput.PerformLayout();
+            this.grbTypes.ResumeLayout(false);
+            this.grbTypes.PerformLayout();
+            this.grbOptions.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.RadioButton rbnEncode;
+        private System.Windows.Forms.GroupBox grbOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.RadioButton rbnDecode;
+        private System.Windows.Forms.GroupBox grbTypes;
+        private System.Windows.Forms.GroupBox grbOptions;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            //Array for the more code alphabet.
+            //Array for the mores code alphabet represented by ASCII, each line is 10 ASCII digits.
+            //Characters from 96+ were not needed because no morsecode exists for them.
+            //Characters with no morese code below 95 have been left blank.
+            string[] Morse =
+                {
+                    "", "", "", "", "", "", "", "", "", "", //0-9
+                    "", "", "", "", "", "", "", "", "", "", //10-19
+                    "", "", "", "", "", "", "", "", "", "", //20-29
+                    "", "", "  ", "-·-·--", "·-··-·", "", "···-··-", "", "·-···", "·----·", //30-39
+                    "-·--·", "-·--·-", "-..-", "·-·-·", "--··--", "-····-", "·-·-·-", "-··-·", "-----", ".----",  //40-49
+                    "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "---...", "-.-.-.",  //50-59
+                    "", "-...-", "", "..--..", ".--.-.",  ".-", "-...", "-.-.", "-..", ".", //60-69
+                    "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", //70-79
+                    ".--.", "--.-", ".-.",  "...", "-", "..-", "...-", ".--", "-..-", "-.--", //80-89
+                    "--..", "", "", "", "","··--·-",  //90-95
+                };
+            String Message = "";
+
+            //Encoding code.
+            if (rbnEncode.Checked)
+            {
+                //Repeates for every letter entered.
+                foreach (char c in txtInput.Text.ToUpper())
+                {
+                    //Converts letters to ASCII then takes the first ASCII number away to start from 0.
+                    Message = Message + " " + Morse[System.Convert.ToInt32(c)];
+                }
+            }
+
+            //Decoding code. 
+            else
+            {
+                //Converts the input into an array of the words entered.
+                string[] words = txtInput.Text.Split(' ');
+                for (int i=0; i < words.Length; i++)
+                {
+                    try
+                    {
+                        //Checks for spaces and fixes the output.
+                        if (words[i] == "")
+                        {
+                            Message = Message + " ";
+                        }
+                        else
+                        {
+                            //Searchs the array for input and converts the position to the ASCII letter.
+                            int index1 = Array.IndexOf(Morse, words[i].ToUpper());
+                            char character = (char)index1;
+                            string text = character.ToString();
+                            Message = Message + text;
+                        }
+                    }
+                    catch
+                    {
+                        Message = Message;
+                    }
+                }
+            }
+            //Output Code
+            txtOutput.Text = Message;
+        }
+        private void switchIt()
+        {
+            if (rbnEncode.Checked)
+            {
+                //Swaps the labels around.
+                grbInput.Text = "Input Text";
+                grbOutput.Text = "Output Morse code";
+            }
+            else
+            {
+                //Swaps the labels around.
+                grbInput.Text = "Input Morse Code";
+                grbOutput.Text = "Output Text";
+            }
+            //Wipes text boxes
+            txtInput.Text = null;
+            txtOutput.Text = null;
+        }
+
+
+        //Used to play audio for the code.
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //By default txtoutput always has the morse in it.
+            Char[] MorseToPlay = txtOutput.Text.ToCharArray();
+            
+            //Just incase it is in the input box.
+            if (rbnDecode.Checked)
+            {
+                MorseToPlay = txtInput.Text.ToCharArray();
+            }
+
+            //Runs for as many characters there are.
+            for (int i = 0; i < MorseToPlay.Length; i++)
+            {
+                //Plays dot sound.
+                if (MorseToPlay[i] == '.')
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.Short);
+                    player.Play();
+                }
+
+                //Plays dash sound.
+                else if (MorseToPlay[i] == '-')
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.Long);
+                    player.Play();
+                }
+
+                //Executes as a longer wait for a space
+                else
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+                //Leaves pause inbetween each one.
+                System.Threading.Thread.Sleep(900);
+            }
+        }
+        //used to trigger label change.
+        private void rbnEncode_CheckedChanged_1(object sender, EventArgs e)
+        {
+            switchIt(); 
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtOutput.Text);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            switchIt();
+        }
+    }
+}
